5a6fbe3ca76dff1704ef973770104bf99635b069
ZinedineZethro <zethrozinedine@gmail.com>
2024-06-11 09:38:12 +0700

    Created StudentController class, cleaned DemoApplication code.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,8 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cglib.core.Local;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.Students.Students;
 
 @SpringBootApplication
 @RestController
@@ -17,12 +10,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Students> hello(){
-		return List.of(
-			new Students(1L, "Mariam", 21, LocalDate.of(2000, 1, 5), "mariam.jamal@gmail.com")
-		);
 	}
 }
