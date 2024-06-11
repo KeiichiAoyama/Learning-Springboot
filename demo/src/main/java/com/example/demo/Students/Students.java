fed9f5a2e8654eaa72f38b187d540b8b8184a595
ZinedineZethro <zethrozinedine@gmail.com>
2024-06-11 09:22:31 +0700

    Server test, Class creation
    Tested the Springboot server and it's output.
    Created the students class.

---
+++ demo/src/main/java/com/example/demo/Students/Students.java
@@ -0,0 +1,11 @@
+package com.example.demo.Students;
+
+import java.time.LocalDate;
+
+public class Students {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate dob;
+    private String email;
+}
