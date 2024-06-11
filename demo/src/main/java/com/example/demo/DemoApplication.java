adc00cf8f3dd1242041f45cb139868cdf681aa15
ZinedineZethro <zethrozinedine@gmail.com>
2024-06-11 09:32:22 +0700

    Tested a Get request for students class

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Students.Students;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +20,9 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello","World");
+	public List<Students> hello(){
+		return List.of(
+			new Students(1L, "Mariam", 21, LocalDate.of(2000, 1, 5), "mariam.jamal@gmail.com")
+		);
 	}
 }
